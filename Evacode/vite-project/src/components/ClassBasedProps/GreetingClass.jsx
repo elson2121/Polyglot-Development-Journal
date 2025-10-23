b7bed0076a609a802768f221ca9c08aa5a2cc77d
elson2121 <elsonsd21@gmail.com>
2025-10-23 17:48:46 -0300

    adding some functinaolity to the app

--- Evacode/vite-project/src/components/ClassBasedProps/GreetingClass.jsx
+++ Evacode/vite-project/src/components/ClassBasedProps/GreetingClass.jsx
@@ -11,3 +11,15 @@ export default class GreetingClass extends Component {
     )
   }
 }
+
+//  So… Why do we pass values to props in App.jsx?
+// To make components reusable
+// → One component, many uses.
+// To keep data in one place (App.jsx or parent)
+// → Easier to manage, update, or fetch from an API later.
+// To avoid hard-coding values
+// → Your component doesn’t “know” the name — it just displays whatever it’s given.
+// To follow React’s data flow rule: “Parent → Child”
+// → Props are how parents safely send data down to children.
+// To prepare for real apps
+// → In real projects, App.jsx (or a parent) might get data from a database, then pass it via props to display lists, user info, products, etc.
