0627ccfb803c87e240a035a5c7251b2123dd6684
elson2121 <elsonsd21@gmail.com>
2025-10-23 17:14:11 -0300

    using the props for the function only

---
+++ Evacode/vite-project/src/components/FuncGetting/Geeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Geeting(props) {
+  return (
+    <div>
+      <h1>hello {props.name}</h1>
+      <h1>hello {props.name}</h1>
+    </div>
+  )
+}
+
+export default Geeting
