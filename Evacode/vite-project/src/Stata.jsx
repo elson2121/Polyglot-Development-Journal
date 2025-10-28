0d2d1a00c6b808817a2e6c095a33a1ea65de8ef9
elson2121 <elsonsd21@gmail.com>
2025-10-28 18:00:12 -0300

    the end of the update about the state

--- Evacode/vite-project/src/Stata.jsx
+++ Evacode/vite-project/src/Stata.jsx
@@ -3,23 +3,23 @@ import React from 'react'
 function State() {
     const[state,setState]=React.useState(0);
     const setCounttoten=()=>{
-setState(prevState=> prevState+1);
+setState(prevState=> prevState+1);}
   return (
-    
+<> 
     <div>
        <h1 className="text-lg text-gray-700">Simple click counte</h1> 
        </div>
        <div> 
 <p> current count value </p>
-
+  <span>{state}</span>
        </div>
-       <span>{state}</span>
+     
        <button
 
         onClick={setCounttoten}>Set Count to 10 {state}
        </button>
     
-  )
+ </> )
 }
 
 export default State
