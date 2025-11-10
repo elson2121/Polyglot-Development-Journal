0e4532882395c355b7b8e541a1dec70e138fd786
elson2121 <elsonsd21@gmail.com>
2025-11-10 03:37:09 -0300

    update the recnet one data and the file their

--- Evacode/vite-project/src/Filezila.tsx
+++ Evacode/vite-project/src/Filezila.tsx
@@ -6,6 +6,7 @@ export default class Filezila extends Component {
       <div>
 <h1>hello  workld  </h1>
 <h6>hello   we will hve the examp today  </h6>
+<h2>the  new topic ofr the new data </h2>
 <p>the  </p>
     
       </div>
