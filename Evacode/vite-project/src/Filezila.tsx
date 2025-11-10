2fdac7a51e61a40e7823bd8b07bcf2247a298faf
elson2121 <elsonsd21@gmail.com>
2025-11-10 03:35:21 -0300

    update the recent data

--- Evacode/vite-project/src/Filezila.tsx
+++ Evacode/vite-project/src/Filezila.tsx
@@ -4,7 +4,7 @@ export default class Filezila extends Component {
   render() {
     return (
       <div>
-<h1>hello the workld  </h1>
+<h1>hello  workld  </h1>
 <h6>hello   we will hve the examp today  </h6>
 <p>the  </p>
     
